88c47e934407d1ba0b3e8eb70413e5bf00cfed9e
AglioB <126629977+AglioB@users.noreply.github.com>
2023-03-14 20:29:59 -0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+game 'gta5'
+
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+shared_scripts {
+    'shared/*.lua'
+}
+
+ui_page 'web/index.html'
+
+files {
+    'web/*.*'
+}
